63ceb0be1cf3416d999a15a4362665884d601d2c
Rafael Alan <admsoad15@hotmail.com>
2025-03-23 18:11:55 -0300

    Validação da inserção dos produtos

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,9 +4,22 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string descricao;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao { 
+            get=> descricao;
+            set
+            {
+                if(value == null) 
+                {
+                    throw new Exception("Por favor preencha a descrição");
+                }
+
+                descricao = value;
+            }
+        }
         public double Quantidade {  get; set; }
         public double Preco {  get; set; }
         public double Total { get => Quantidade * Preco; }
