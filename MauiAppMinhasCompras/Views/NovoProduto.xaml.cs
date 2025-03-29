048f41a3d693a47a80e6107ccda275d0b64ba470
Rafael Alan <admsoad15@hotmail.com>
2025-03-29 19:22:58 -0300

    Melhoras na interface gráfica

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -4,31 +4,30 @@ namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
 {
-	public NovoProduto()
-	{
-		InitializeComponent();
-	}
+    public NovoProduto()
+    {
+        InitializeComponent();
+    }
 
     private async void ToolbarItem_Clicked(object sender, EventArgs e)
     {
-		try
-		{
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
 
-			Produto p = new Produto
-			{
-				Descricao = txt_descricao.Text,
-				Quantidade = Convert.ToDouble(txt_quantidade.Text),
-				Preco = Convert.ToDouble(txt_preco.Text)
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+            await Navigation.PopAsync();
 
-			};
-
-			await App.Db.Insert(p);
-			await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
-
-		}
-		catch (Exception ex)
-		{
-			await DisplayAlert("Ops", ex.Message, "OK");
-		}
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
